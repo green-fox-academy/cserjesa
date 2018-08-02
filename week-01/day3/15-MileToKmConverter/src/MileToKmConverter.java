e1693949f665417b524bd9603de416d48aa654c6
cserjesa <andras@cserjes.com>
2018-08-02 18:28:45 +0200

    feat(MileToKmConverter): formatting

--- week-01/day3/15-MileToKmConverter/src/MileToKmConverter.java
+++ week-01/day3/15-MileToKmConverter/src/MileToKmConverter.java
@@ -4,10 +4,10 @@ public class MileToKmConverter {
         // Write a program that asks for an integer that is a distance in kilometers,
         // then it converts that value to miles and prints it
 
-        System.out.println("Hey I can convert kilometers to miles" + "\n" + "What do you want me to convert?");
+        System.out.println("Hey I can convert kilometers to miles\nWhat do you want me to convert?");
         Scanner scanner = new Scanner(System.in);
         double userInput1 = scanner.nextInt();
 
-        System.out.println(userInput1 + " km equals " + (0.621371192 * userInput1) + " miles.");
+        System.out.println((int)userInput1 + " km equals " + (0.621371192 * userInput1) + " miles.");
     }
 }

87a95e0910db65815c011fe8097026cde7a115ae
cserjesa <andras@cserjes.com>
2018-08-02 16:54:49 +0200

    feat(OddEven): finished

---
+++ week-01/day3/18-OddEven/src/OddEven.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OddEven {
+
+// Write a program that reads a number from the standard input,
+// Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me the number:");
+        int u1 = scanner.nextInt();
+
+    if((u1 % 2) != 0) {
+        System.out.println(u1 + " is odd.");
+    }
+    else {
+        System.out.println(u1 + " is even.");
+    }
+    }
+
+
+}

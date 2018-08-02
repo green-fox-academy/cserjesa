9f22bb5cf9efb019e9bb1f191057884f45ec51ff
cserjesa <andras@cserjes.com>
2018-08-02 16:14:05 +0200

    feat(MileToKmConverter): finished

---
+++ week-01/day3/15-MileToKmConverter/src/MileToKmConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MileToKmConverter {
+    public static void main(String[] args) {
+        // Write a program that asks for an integer that is a distance in kilometers,
+        // then it converts that value to miles and prints it
+
+        System.out.println("Hey I can convert from kilometers to miles" + "\n" + "What do you want me to convert?");
+        Scanner scanner = new Scanner(System.in);
+        double userInput1 = scanner.nextInt();
+
+        System.out.println(userInput1 + " km equals to " + (0.621371192 * userInput1) + " miles.");
+    }
+}

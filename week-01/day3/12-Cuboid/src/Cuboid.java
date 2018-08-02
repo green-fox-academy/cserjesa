4b55cb620806c2d9d8cda27fa6bbcb0cafce320f
cserjesa <andras@cserjes.com>
2018-08-02 18:19:14 +0200

    feat(Cuboid): formatting

--- week-01/day3/12-Cuboid/src/Cuboid.java
+++ week-01/day3/12-Cuboid/src/Cuboid.java
@@ -1,6 +1,7 @@
 public class Cuboid {
     public static void main(String[] args) {
-        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
 // The program should write the surface area and volume of the cuboid like:
 //
 // Surface Area: 600

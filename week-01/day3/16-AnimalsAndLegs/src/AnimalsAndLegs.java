e65a532ebb3c843eb1820bccda95cf5930efa0b3
cserjesa <andras@cserjes.com>
2018-08-02 16:31:48 +0200

    feat(AnimalsAndLegs): finished

---
+++ week-01/day3/16-AnimalsAndLegs/src/AnimalsAndLegs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AnimalsAndLegs {
+    public static void main(String[] args) {
+        // Write a program that asks for two integers
+        // The first represents the number of chickens the farmer has
+        // The second represents the number of pigs owned by the farmer
+        // It should print how many legs all the animals have
+
+        System.out.println("Hello farmer!" + "\n" + "How man chickens have you got? (I mean animals.)");
+        Scanner scanner = new Scanner(System.in);
+        int userInput1 = scanner.nextInt();
+        System.out.println("How many pigs?");
+        int userInput2 = scanner.nextInt();
+
+        System.out.println("Dear Farmer \nYour animals have " +
+                ((2 * userInput1) + (4 * userInput2)) + " legs.\nKeep in mind!");
+    }
+}

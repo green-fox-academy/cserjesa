c61686011af424754234c684eb2a0af918d92c0d
cserjesa <andras@cserjes.com>
2018-08-02 12:10:02 +0200

    day3 stuff

--- week-01/day3/01-HelloMe/src/HelloMe.java
+++ week-01/day3/01-HelloMe/src/HelloMe.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class HelloMe {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, cserjesa!");
+    }
 }

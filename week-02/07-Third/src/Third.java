a0efd5120e41a0e8c4068d467b7c65a8207f0da3
cserjesa <andras@cserjes.com>
2018-08-06 16:28:30 +0200

    feat(Third): finished

---
+++ week-02/07-Third/src/Third.java
@@ -0,0 +1,17 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+public class Third {
+
+    public static void main(String[] args) {
+        int[] q = {4, 5, 6, 7};
+
+        // Print the second element of the array
+        System.out.println("The third element is " + q[2]);
+
+        // Print all the array elements
+        for (int i = 0; i < q.length; i++) {
+        }
+    }
+}

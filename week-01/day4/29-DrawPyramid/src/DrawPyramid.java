d7bc37935e5103f04eca2512e314aabcb370f939
cserjesa <andras@cserjes.com>
2018-08-07 01:01:27 +0200

    feat(DrawPyramid): finished

--- week-01/day4/29-DrawPyramid/src/DrawPyramid.java
+++ week-01/day4/29-DrawPyramid/src/DrawPyramid.java
@@ -14,16 +14,14 @@ public class DrawPyramid {
         System.out.println("I'll draw you a pyramid! What size you want? Type a number please.");
         Scanner scanner = new Scanner(System.in);
         int u1 = scanner.nextInt();
-        for (int i = 1; i <= u1 * 2; i += 2) {
-            for (int star = 1; star <= i; star++) {
-                for (int space = u1 / 2; space <= 0; space -= 1) {
-                    System.out.println(space);
-                    System.out.print(" ");
-                }
+        for (int i = 1; i <= u1; i++) {
+            for (int space = u1 - i; space >= 1; space--) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= (i * 2) - 1; star++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
-
 }

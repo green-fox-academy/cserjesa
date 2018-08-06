4738acd8b109bf11cde555f38f07e0d0e85abb3b
cserjesa <andras@cserjes.com>
2018-08-06 17:45:31 +0200

    style(day2): move into correct folder

---
+++ week-02/day1/13-Matrix/src/Matrix.java
@@ -0,0 +1,27 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
+
+import java.util.Arrays;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 1;
+            }
+            System.out.println(Arrays.toString(matrix));
+        }
+        System.out.println();
+    }
+}
+

7da75e40fe735e5216f8c83908efeeb5f6d4d10b
cserjesa <andras@cserjes.com>
2018-08-08 15:42:13 +0200

    feat(SquarePositioning): Finished

---
+++ week-02/day3/08-SquarePositioning/src/Drawing.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+
+        int x[] = {50, 110, 170};
+        int y[] = {50, 110, 170};
+        for (int sor = 0; sor < 3; sor++) {
+            toCenter(graphics, x[sor], y[sor]);
+        }
+    }
+
+    public static void toCenter(Graphics graphics, int x, int y) {
+        graphics.fillRect(x, y, 50, 50);
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+}

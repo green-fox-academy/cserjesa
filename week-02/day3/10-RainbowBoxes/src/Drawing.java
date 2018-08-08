fd6a44bae26a5c8d40d7ff7a3713aa8244fcc651
cserjesa <andras@cserjes.com>
2018-08-08 16:26:01 +0200

    feat(RainbowBoxes): Finished

---
+++ week-02/day3/10-RainbowBoxes/src/Drawing.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+    public static void mainDraw(Graphics graphics) {
+        // Create a square drawing function that takes 2 parameters:
+        // The square size, and the fill color,
+        // and draws a square of that size and color to the center of the canvas.
+        // Create a loop that fills the canvas with rainbow colored squares.
+
+        for (int c = 255; c >0; c -= 15) {
+            Color myColor = new Color(c, 255-c, c/2);
+            toCenter(graphics, c, myColor);
+        }
+    }
+
+    public static void toCenter(Graphics graphics, int x, Color colors) {
+        graphics.setColor(colors);
+        graphics.fillRect(150 - x / 2, 147 - x / 2, x, x);
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}

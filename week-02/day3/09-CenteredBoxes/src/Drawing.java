508868d611f3a4601fab993e89b00d58168591e3
cserjesa <andras@cserjes.com>
2018-08-08 23:07:03 +0200

    Refactor(CenteredBoxes): Engish variables

--- week-02/day3/09-CenteredBoxes/src/Drawing.java
+++ week-02/day3/09-CenteredBoxes/src/Drawing.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
-
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Drawing {
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
 
     public static void mainDraw(Graphics graphics) {
         // create a square drawing function that takes 1 parameter:
@@ -14,19 +17,14 @@ public class Drawing {
         // avoid code duplication.
 
         int rectangles[] = {50, 100, 150};
-        for (int sor = 0; sor < 3; sor++) {
-            toCenter(graphics, rectangles[sor]);
+        for (int j = 0; j < 3; j++) {
+            toCenter(graphics, rectangles[j]);
         }
     }
 
     public static void toCenter(Graphics graphics, int x) {
         graphics.drawRect(150 - x / 2, 147 - x / 2, x, x);
     }
-
-
-    //    Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 343;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");

cfcf2c018fe81624a4d93c3e9560d327e67c81ad
cserjesa <andras@cserjes.com>
2018-08-15 14:41:11 +0200

    feat:(BlogPost) Finished

---
+++ week-03/day2/02-BlogPost/src/BlogPost.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class BlogPost {
+    String authorName;
+    String title;
+    String text;
+    String publicationDate;
+
+
+    BlogPost(String authorName, String title, String text, String publicationDate) {
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+    }
+}

cc882e1b1f9d5a3895b28f555d4f61f16f724f5a
cserjesa <andras@cserjes.com>
2018-08-17 13:04:23 +0200

    feat:(numberAdder) another version with class level variable

--- week-03/day3/02-numberAdder/src/numberAdder.java
+++ week-03/day3/02-numberAdder/src/numberAdder.java
@@ -1,16 +1,17 @@
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
 public class numberAdder {
+    static int counter = 88;
     public static void main(String args[]) {
-        int counter = 88;
+
         int n =1;
-        couterMethod(n, counter);
+        couterMethod(n);
     }
 
-    private static void couterMethod(int n, int counter) {
+    private static void couterMethod(int n) {
         if (n >= counter) return;
         System.out.println(n);
         n++;
-        couterMethod(n, counter);
+        couterMethod(n);
     }
 }

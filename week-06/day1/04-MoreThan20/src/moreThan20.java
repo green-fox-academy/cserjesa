6e9c7cd3945d4f5980d795101610891f18026033
cserjesa <andras@cserjes.com>
2018-09-11 17:43:42 +0200

    Feat(04) Finished

--- week-06/day1/04-MoreThan20/src/moreThan20.java
+++ week-06/day1/04-MoreThan20/src/moreThan20.java
@@ -3,8 +3,14 @@ import java.util.Arrays;
 
 public class moreThan20 {
 
-//    Write a Stream Expression to find which number squared value is more then 20 from the following array:
+    public static void main(String[] args) {
 
-    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
-    
+//    Write a Stream Expression to find which number squared value is more then 20 from the following array:
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+        numbers.stream()
+                .filter(n -> n > 0)
+                .map(n -> Math.pow(n, 2))
+                .filter(n -> n > 20)
+                .forEach(System.out::println);
+    }
 }

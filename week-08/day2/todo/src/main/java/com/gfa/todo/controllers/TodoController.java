7592cf264a1aabc0580ecbe0769d2af6dd5183e3
cserjesa <andras@cserjes.com>
2018-09-25 17:56:54 +0200

    feat:(TodoWeb) Delete ready

--- week-08/day2/todo/src/main/java/com/gfa/todo/controllers/TodoController.java
+++ week-08/day2/todo/src/main/java/com/gfa/todo/controllers/TodoController.java
@@ -5,10 +5,12 @@ import com.gfa.todo.interfaces.TodoRepository;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -24,17 +26,54 @@ public class TodoController {
     public String list(@RequestParam(required = false) Boolean isActive, Model model) {
         if (isActive == null) {
             model.addAttribute("todos", todoRepository.findAll());
-        } else if (isActive){
-            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+        } else if (isActive) {
+            model.addAttribute("todos", Streamable.of(todoRepository.findAll()).stream()
                     .filter(a -> !a.isDone())
-                    .collect(Collectors.toList());
-            model.addAttribute("todos", isNotDone);
+                    .collect(Collectors.toList()));
         } else if (!isActive) {
-            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+            model.addAttribute("todos", Streamable.of(todoRepository.findAll()).stream()
                     .filter(a -> a.isDone())
-                    .collect(Collectors.toList());
-            model.addAttribute("todos", isNotDone);
+                    .collect(Collectors.toList()));
         }
         return "todoslist";
     }
+
+    @RequestMapping("/add")
+    public String add(Model model) {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addPost(String title, Model model) {
+        todoRepository.save(new Todo(title));
+        return "redirect:/";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deletePost(Model model, @PathVariable Long id) {
+        Optional<Todo> todo = todoRepository.findById(id);
+        if (todo.isPresent()) {
+            todoRepository.delete(todo.get());
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable Long id) {
+        model.addAttribute("title", todoRepository.findById(id));
+        return "/{id}/edit";
+    }
+
+    @RequestMapping("/edit")
+    public String edit2(Model model, @PathVariable Long id) {
+        model.addAttribute("title", todoRepository.findById(id));
+        return "/{id}/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editPost(Model model, @PathVariable Long id) {
+ //       todoRepository.save(new Todo(title));
+        return "/edit";
+    }
+
 }

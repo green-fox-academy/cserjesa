5a59b19d8f0f49116bd476ddad9d3c86f4eb39bf
cserjesa <andras@cserjes.com>
2018-09-25 16:05:59 +0200

    feat:(TodoWeb) 60% done

--- week-08/day2/todo/src/main/java/com/gfa/todo/controllers/TodoController.java
+++ week-08/day2/todo/src/main/java/com/gfa/todo/controllers/TodoController.java
@@ -1,14 +1,19 @@
 package com.gfa.todo.controllers;
 
+import com.gfa.todo.Todo;
 import com.gfa.todo.interfaces.TodoRepository;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class TodoController {
-    TodoRepository todoRepository;
+    private TodoRepository todoRepository;
 
     public TodoController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
@@ -16,11 +21,19 @@ public class TodoController {
 
 
     @RequestMapping(value = {"/list", "/"})
-    public String list(@RequestParam(required = false) Boolean active, Model model) {
-        if (active != null) {
+    public String list(@RequestParam(required = false) Boolean isActive, Model model) {
+        if (isActive == null || !isActive) {
             model.addAttribute("todos", todoRepository.findAll());
-        } else if (active = true) {
-            model.addAttribute("todos",)
+        } else if (isActive){
+            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+                    .filter(a -> !a.isDone())
+                    .collect(Collectors.toList());
+            model.addAttribute("todos", isNotDone);
+        } else if (isActive) {
+            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+                    .filter(a -> a.isDone())
+                    .collect(Collectors.toList());
+            model.addAttribute("todos", isNotDone);
         }
         return "todoslist";
     }
